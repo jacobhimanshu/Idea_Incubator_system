43f8e8b3ddf3437d95fe0c70d7f9aca085b87afc
Himanshu Maurya <himanshumaurya@5854>
2025-10-04 22:17:32 +0530

    third  commit

--- src/pages/Editidea.jsx
+++ src/pages/Editidea.jsx
@@ -1,9 +1,9 @@
 
 
-import React, { useEffect, useState } from "react";
+import  { useEffect, useState } from "react";
 import { useParams, useNavigate } from "react-router-dom";
 import axios from "axios";
-import { Idea } from "../../../backend/src/models/idea.model";
+// import { Idea } from "../../../backend/src/models/idea.model";
 
 const EditIdea = () => {
   const { id } = useParams(); 
